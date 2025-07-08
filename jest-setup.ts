220897bb9cd956435cdfc584ff0f4b3fbdcebb27
MLGRussianXP <daniilkoles47@gmail.com>
2025-07-08 10:02:51 +0500

    chore: improve test setup with cleanup and timer management

--- jest-setup.ts
+++ jest-setup.ts
@@ -5,3 +5,14 @@ import '@testing-library/react-native/extend-expect';
 global.window = {};
 // @ts-ignore
 global.window = global;
+
+// Clean up after each test
+afterEach(() => {
+  jest.clearAllTimers();
+  jest.clearAllMocks();
+});
+
+// Ensure all timers are cleaned up
+afterAll(() => {
+  jest.useRealTimers();
+});

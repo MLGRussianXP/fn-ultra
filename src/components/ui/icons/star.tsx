cef23b9df1fa23d8585c14d85947d28df869c00c
MLGRussianXP <daniilkoles47@gmail.com>
2025-07-23 12:51:46 +0500

    feat(icons): clear settings page
    Deleted the Rate, Share, Support, and
    Website icon components to streamline
    the icon library. Introduced the Star
    icon in the settings screen, allowing
    users to star the GitHub repository.
    Updated translations.

---
+++ src/components/ui/icons/star.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import type { SvgProps } from 'react-native-svg';
+import Svg, { Path } from 'react-native-svg';
+
+import colors from '../colors';
+
+export const Star = ({ color = colors.neutral[500], ...props }: SvgProps) => (
+  <Svg width={24} height={24} fill="none" viewBox="0 0 24 24" {...props}>
+    <Path
+      d="M12 2l2.4 7.4h7.6l-6.2 4.5 2.4 7.4-6.2-4.5-6.2 4.5 2.4-7.4-6.2-4.5h7.6z"
+      stroke={color}
+      strokeWidth={1.219}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Svg>
+);
